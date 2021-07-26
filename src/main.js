a3d4a6e6864753fb01d68d26ddb8d171d3d3ecc7
wololo456 <angelsantosmahedero@gmail.com>
2021-07-26 15:54:19 +0200

    my first222

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
-App.listen(process.env.PORT || 8080);
+App.listen(process.env.PORT || 3000);
 
 new Vue({
   render: h => h(App),
